82a96317d0c1d118b7107b5fadd4ae07270bdc93
droru <droruri3@gmail.com>
2020-04-10 15:10:38 +0300

    init project

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ let cors = require('cors');
 app.use(bodyParser.urlencoded({extend: true}));
 app.use(bodyParser.json());
 app.use(cors());
-
+app.use(express.static('public'));
+app.use((req, res, next) => {
+    res.show = (name) => {
+        res.sendFile(`/client/${name}`, {root: __dirname});
+    };
+    next();
+});
 
 //Post request -> get data from client by req.body."element"
-app.post("/getHello", async function (req, res) {
-    console.log(req.body)
-    let data = {
-        key : "val"
-    }
-    res.send(data)
-
+app.get('/', (req, res) => {
+    res.show("welcome.html");
 });
+
 
 
 
--- client/script.js
+++ client/script.js
@@ -22,8 +22,12 @@ async function postData(url = '', data = {}) {
 }
 
 //if you want to send data
-postData('http://localhost:3000/getHello', { answer: 42 ,
+/*
+postData('http://localhost:3000/getThanks', { answer: 42 ,
 user : "me"})
     .then((data) => {
         console.log(data); // JSON data parsed by `response.json()` call
     });
+
+
+ */
